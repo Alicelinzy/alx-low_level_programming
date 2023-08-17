0714651b473c0ea4ed33618deef1683b26d8e576
alice linzy <linzyalice89@gmail.com>
2023-08-17 12:18:51 -0700

    c is fun

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,12 +21,12 @@ printf("FizzBuzz");
 else if (i % 3 == 0)
 printf("Fizz");
 else if (i % 5 == 0)
-rintf("Buzz");
+printf("Buzz");
 else
 printf("%i", i);
 if (i < 100)
 printf(" ");
-	}
+}
 printf("\n");
 return (0);
 }
